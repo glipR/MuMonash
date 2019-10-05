2a83a36ead30b8ddfd08a70fe2f835708b90f765
glipR <jgoe0001@student.monash.edu>
2019-10-05 19:38:24 +1000

    Add some (possibly faulty) code for Range updates on segtree.

--- BinderAlgos/segtree.cpp
+++ BinderAlgos/segtree.cpp
@@ -6,6 +6,12 @@
 using namespace std;
 
 typedef vector<int> vi;
+typedef long long ll;
+
+ll gcd(ll a, ll b) {
+    if (b==0) { return (a<0) ? -a: a; }
+    else { return gcd(b, a%b); }
+}
 
 // Static RMQ (Sparse Table)
 // Can solve Range Queries for:
@@ -57,6 +63,67 @@ template <typename T> struct SegmentTree {
     }
 };
 
+// Dynamic RMQ (Segment Tree) with range updates.
+// More modular, and allows for Custom SegTree "Parts" to be added in.
+// As above, O(N) build, and O(log(n)) query.
+// Here, U is that "part" mentioned above.
+template<typename T, typename U> struct SegmentTree2 {
+    T I, t[4]; int N, h; vector<T> A; // I is the identity value.
+    SegmentTree2(const vector<T>& data, T I=T()): I(I), N(data.size()),
+    h(sizeof(int)*8-__builtin_clz(N)), A(2*N, I) {
+        copy(data.begin(), data.end(), A.begin()+N);
+        for (int i=N-1; i;i--) op(i);
+    }
+    void op(int i) { A[i].op(A[2*i], A[2*i+1]); }
+    void prop(int i) { A[2*i].us(A[i].U); A[2*i+1].us(A[i].U); A[i].NU(); }
+    void push(int i) {for (int j=h;j;j--) prop(i>>j); }
+    void update(int l, int r, U v) { // Acts on [l, r)
+        push(l+=N); push((r+=N)-1); bool cl=0,cr=0;
+        for (;l<r;l/=2,r/=2) {
+            if (cl) op(l-1); if (cr) op(r);
+            if (l&1) A[l++].us(v), cl=1;
+            if (r&1) A[--r].us(v), cr=1;
+        }
+        if (l==1 && cr) op(1);
+        else for (l--;r>0;l/=2,r/=2) {
+            if (cl && l) op(l);
+            if (cr && (!cl || (l!=r && r!=1))) op(r);
+        }
+    }
+    T query(int l, int r) { // Acts on [l, r)
+        push(l+=N); push((r+=N)-1);
+        t[0]=t[2]=I; int i=0, j=2;
+        for(;l<r;l/=2,r/=2) {
+            if (l&1) t[i^1].op(t[i],A[l++]), i^=1;
+            if (r&1) t[j^1].op(A[--r],t[j]), j^=1;
+        }
+        t[i^1].op(t[i],t[j]);
+        return t[i^1];
+    }
+};
+
+struct RangeGCD {  // Example Input to above, to get GCD over range and update value over range.
+    // THIS IS BUGGED OR THE IMPLEMENTATION ABOVE IS BUGGED
+    int a = INT_MIN, U = INT_MAX;
+    void op(RangeGCD& b, RangeGCD& c) { a=gcd(b.a, c.a); }  // Merge two segments.
+    void us(int v) { if (v!=INT_MAX) a=U=v; }  // Apply a lazy update (Possibly worse than actuality?)
+    void NU() { U = INT_MAX; }  // Node requires no update?
+};
+
+struct RangeSum {
+    int a = 0, U = INT_MAX;
+    void op(RangeSum& b, RangeSum& c) { a = b.a + c.a; }
+    void us(int v) { if (v!=INT_MAX) a=U=v; }
+    void NU() { U=INT_MAX; }
+};
+
+struct RangeMin {
+    int a = INT_MAX, U = INT_MIN;
+    void op(RangeMin& b, RangeMin& c) { a=min(b.a, c.a); }
+    void us(int v) { if (v!=INT_MIN) a=U=v; }
+    void NU() { U=INT_MIN; }
+};
+
 int main() {
 
     vector<int> list;
@@ -72,6 +139,8 @@ int main() {
     SparseTable<int> s(list);
     SegmentTree<int> d(list);
 
+    cout << "Static vs Dynamic Min" << endl;
+
     cout << s.query(3, 7).first << " " << s.query(3, 7).second << endl;
     cout << d.query(3, 7).first << " " << d.query(3, 7).second << endl;
     cout << s.query(0, 2).first << " " << s.query(0, 2).second << endl;
@@ -79,6 +148,16 @@ int main() {
     d.update(3, 1);
     cout << s.query(2, 3).first << " " << s.query(2, 3).second << endl;
     cout << d.query(2, 3).first << " " << d.query(2, 3).second << endl;
+
+    cout << "GCD Range Updates" << endl;
+
+    SegmentTree2<RangeMin, int> st(vector<RangeMin>(10));
+    st.update(0, 4, 8);
+    st.update(4, 5, 6);
+    st.update(5, 10, 16);
+    cout << st.query(4, 10).a << endl;
+    cout << st.query(0, 6).a << endl;
+    cout << st.query(7, 10).a << endl;
 
     return 0;
 }
