2243a8f3e4fff0237ed50f6db99f1d16618dbaf0
Alireza Khoshghalb <alirezakhoshghalb@ymail.com>
2019-10-19 11:36:10 +1100

    Lexical

---
+++ IEEEXtreme13/Lexical Addition/Lexical Addition.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+ll result[100001];
+ll n, a, b;
+
+bool check(ll len)
+{
+    ll sum = len * a;
+    for (int i = len - 1; i >= 0; i--)
+    {
+        if (sum >= n) break;
+        result[i] = min(b, n-sum);
+        sum += min(b, n-sum);
+    }
+}
+
+int main()
+{
+    cin >> n >> a >> b;
+    memset(result, sizeof(result), a);
+    ll l = 0, r = 100001, len = -1;
+    for (int i = 1; i <= 100000; i++)
+    {
+        ll lower = i * a;
+        ll upper = i * b;
+        if (n >= lower && n <= upper)
+        {
+            len = i;
+            break;
+        }
+    }
+    if (len == -1) { cout << "NO" << endl; }
+    else {
+        cout << "YES" << endl;
+        check(len);
+        for (int i = 0; i < len; i++)
+            cout << result[i] << ' ';
+        cout << endl;
+    }
+}
+
