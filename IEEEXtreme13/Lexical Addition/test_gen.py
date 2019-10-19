354a77bdddfca607310da1713c49b761c457bf25
glipR <jgoe0001@student.monash.edu>
2019-10-19 12:06:37 +1100

    Change tester.

--- IEEEXtreme13/Lexical Addition/test_gen.py
+++ IEEEXtreme13/Lexical Addition/test_gen.py
@@ -1,7 +1,7 @@
 import random
 
-N = 1000000000000000
+N = 10000000000
 
 # Randomly generated
-a = random.randint(1, 10000)
+a = random.randint(1, 100000)
 print(random.randint(1, N), a, random.randint(a, 1000000000))
