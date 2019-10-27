429e25cf88bc23578ab84135ce6ea3c3bdcae9ac
glipR <jgoe0001@student.monash.edu>
2019-10-28 08:54:23 +1100

    Push some changes for E.cpp, still WA.

--- JakartaRegional/E/E.cpp
+++ JakartaRegional/E/E.cpp
@@ -21,44 +21,65 @@ int main() {
         cin >> orig_melody[i];
     }
 
-    ranges[0] = pair<ll, ll>(l, r);
-    for (int i=0; i<n-1; i++) {
-        pair<ll, ll> new_range(max(ranges[i].first - k, l), min(ranges[i].second + k, r));
-        // Adjust the range for the next note.
-        if (orig_melody[i] < orig_melody[i+1]) {
-            // B[i] < B[i+1]
-            new_range.first = max(ranges[i].first + 1, new_range.first);
-        } else
-        if (orig_melody[i] == orig_melody[i+1]) {
-            new_range.first = max(ranges[i].first, new_range.first);
-            new_range.second = min(ranges[i].second, new_range.second);
-        } else {
-            // B[i] > B[i+1]
-            new_range.second = min(ranges[i].second - 1, new_range.second);
-        }
-        ranges[i+1] = new_range;
-    }
-
-    // Go back to limit the original options.
-    for (int i=n; i>0; i--) {
-        if (orig_melody[i-1] < orig_melody[i]) {
-            // B[i-1] < B[i]
-            ranges[i-1].second = min(ranges[i].second - 1, ranges[i-1].second);
-        } else
-        if (orig_melody[i-1] == orig_melody[i]) {
-            // B[i-1] == B[i]
-            ranges[i-1].first = max(ranges[i].first, ranges[i-1].first);
-            ranges[i-1].second = min(ranges[i].second, ranges[i-1].second);
-        } else {
-            // B[i-1] > B[i]
-            ranges[i-1].first = max(ranges[i].first + 1, ranges[i-1].first);
-        }
-    }
-
+    bool changed = true;
     bool bad = false;
+    int iterations = 0;
+
+    for (int i=0; i<n; i++)
+        ranges[i] = pair<ll, ll>(l, r);
+    while (changed && (!bad)) {
+        iterations ++;
+        changed = false;
+        for (int i=0; i<n-1; i++) {
+            pair<ll, ll> old_range = ranges[i+1];
+            ranges[i+1].first = max(ranges[i].first - k, ranges[i+1].first);
+            ranges[i+1].second = min(ranges[i].second + k, ranges[i+1].second);
+            // Adjust the range for the next note.
+            if (orig_melody[i] < orig_melody[i+1]) {
+                // B[i] < B[i+1]
+                ranges[i+1].first = max(ranges[i].first + 1, ranges[i+1].first);
+            } else
+            if (orig_melody[i] == orig_melody[i+1]) {
+                ranges[i+1].first = max(ranges[i].first, ranges[i+1].first);
+                ranges[i+1].second = min(ranges[i].second, ranges[i+1].second);
+            } else {
+                // B[i] > B[i+1]
+                ranges[i+1].second = min(ranges[i].second - 1, ranges[i+1].second);
+            }
+            if (old_range != ranges[i+1]) changed = true;
+        }
+
+        // Go back to limit the original options.
+        for (int i=n-1; i>0; i--) {
+            pair<ll, ll> old_range = ranges[i-1];
+            // Limit by range K.
+            ranges[i].first = max(ranges[i-1].first - k, ranges[i].first);
+            ranges[i].second = min(ranges[i-1].second + k, ranges[i].second);
+            if (orig_melody[i-1] < orig_melody[i]) {
+                // B[i-1] < B[i]
+                ranges[i-1].second = min(ranges[i].second - 1, ranges[i-1].second);
+            } else
+            if (orig_melody[i-1] == orig_melody[i]) {
+                // B[i-1] == B[i]
+                ranges[i-1].first = max(ranges[i].first, ranges[i-1].first);
+                ranges[i-1].second = min(ranges[i].second, ranges[i-1].second);
+            } else {
+                // B[i-1] > B[i]
+                ranges[i-1].first = max(ranges[i].first + 1, ranges[i-1].first);
+            }
+            if (old_range != ranges[i-1]) changed = true;
+        }
+        for (int i=0; i<n; i++) {
+            if (ranges[i].first > ranges[i].second) {
+                bad = true;
+            }
+        }
+    }
+
+    // cerr << iterations << " Iterations" << endl;
+
     // Now pick the lexicographically smallest.
     for (int i=0; i<n; i++) {
-        if (ranges[i].first > ranges[i].second) bad = true;
         if (i == 0) {
             solutions[i] = ranges[i].first;
             continue;
@@ -70,7 +91,7 @@ int main() {
         if (orig_melody[i-1] == orig_melody[i]) {
             solutions[i] = solutions[i-1];
         } else {
-            solutions[i] = max(ranges[i].first, solutions[i-1] - k);
+            solutions[i] = max(max(ranges[i].first, solutions[i-1] - k), l);
         }
     }
 
