a2d3c033cec3aed3d5aac039e7980add84163379
leaen <mossbanay@gmail.com>
2019-11-22 11:07:07 +1100

    ?? confusion

--- ICPCDiv19/C/Cmoss.cpp
+++ ICPCDiv19/C/Cmoss.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <bitset>
 
 using namespace std;
 
@@ -30,68 +31,58 @@ bool mask_met(int s, int mask) {
 
 // returns 1 if alice wins, 0 if bob wins
 int solve(int s) {
+    std::bitset<17> y(s);
     if (dp_state[s] != -1) { return dp_state[s]; }
-
-    // check if assassin is marked
-    if (mask_met(s, assassin_mask)) {
-        // whoevers turn it is now wins since the last player chose the assassin
-        if (is_alice_turn(s)) {
-            dp_state[s] = 1;
-        } else {
-            dp_state[s] = 0;
-        }
-
-        return dp_state[s];
-    }
-
-    // check if blue mask is satisfied
-    if (mask_met(s, blue_mask)) {
-        // alice owns blue so she wins
-        dp_state[s] = 1;
-        return 1;
-    }
-
-    // check if red mask is satisfied
-    if (mask_met(s, red_mask)) {
-        // bob owns red so he wins
-        dp_state[s] = 0;
-        return 0;
-    }
 
     bool player_can_win = false;
     for (int i=0; i<n_hints; ++i) {
         vector<int> this_hints = hints[i];
 
+        int next_s = s;
+
+        // play out this hint
         for (int j=0; j<this_hints.size(); ++j) {
             int guess = this_hints[j];
             int guess_bit = 1 << guess;
-            // has this card already been guessed?
-            if (mask_met(s, guess_bit)) {
-                // cant play again
-                continue;
-            }
 
-            // can at least play this card
-            int next_s = s;
+            if (mask_met(next_s, guess_bit)) {
+                continue; // cant play the same card twice
+            }
 
             next_s |= guess_bit; // mark card as used
 
-            if (is_alice_turn(s)) {
-                if (mask_met(blue_mask, guess_bit)) {} else { next_s ^= 1<<16; }
-            } else {
-                if (mask_met(red_mask, guess_bit)) {} else { next_s ^= 1<<16; }
+            if (is_alice_turn(s) && !mask_met(blue_mask, guess_bit)) {
+                break;
+            } else if (!is_alice_turn(s) && !mask_met(red_mask, guess_bit)) {
+                break;
             }
 
-            int winner_from_here = solve(next_s);
-
-            if (is_alice_turn(s)) {
-                if (winner_from_here==1) { player_can_win = true; }
-            } else {
-                if (winner_from_here==0) { player_can_win = true; }
+            if (mask_met(next_s, blue_mask)) {
+                if (is_alice_turn(s)) { player_can_win = true; }
+                break;
             }
 
-            // since we have tried to play a card now, we must stop
-            break;
+            if (mask_met(next_s, red_mask)) {
+                if (!is_alice_turn(s)) { player_can_win = true; }
+                break;
+            }
+        }
+
+        if (player_can_win) { break; }
+
+        bool stumped = s == next_s; // player is stumped if no guesses were made
+        if (stumped) continue;
+
+        if (mask_met(next_s, assassin_mask)) continue;
+
+        next_s ^= 1<<16; // swap turns
+
+        int winner_from_here = solve(next_s);
+
+        if (is_alice_turn(s)) {
+            if (winner_from_here==1) { player_can_win = true; }
+        } else {
+            if (winner_from_here==0) { player_can_win = true; }
         }
 
         if (player_can_win) { break; }
@@ -119,16 +110,6 @@ int main() {
     for (int i=0; i<16; ++i) {
         cin >> board_hint[i];
     }
-
-    /*
-    // Print out the board
-    for (int i=0; i<4; ++i) {
-        for (int j=0; j<4; ++j) {
-            cout << board_hint[4*i + j] << " ";
-        }
-        cout << endl;
-    }
-    */
 
     // Read in the masks
     for (int i=0; i<16; ++i) {
