5c77cc5d2f1e8af983a58fccc4f217274c35feb8
glipR <jgoe0001@student.monash.edu>
2019-11-24 10:51:43 +1100

    Golf B.

--- ICPCReg19/B/B.py
+++ ICPCReg19/B/B.py
@@ -1,15 +1,4 @@
-# MuMonash
-n, b = map(int, input().split())
-values = sorted(list(map(int, input().split())))
-DP = [[-1 for _ in range(b+1)] for __ in range(n+1)]
-def solve(i, j):
-    # print(i, j)
-    if DP[i][j] != -1: return DP[i][j]
-    # Try assigning a bucket across all remaining prefixes of this substring
-    if j == 0: return 1e18
-    best = pow((values[-1] - values[i]),2)
-    for x in range(i+1, n):
-        best = min(best, pow(values[i] - values[x-1], 2) + solve(x, j-1))
-    DP[i][j] = best
-    return DP[i][j]
+import functools, sys
+(n, b), values, foo = map(int, input().split()), sorted(list(map(int, input().split()))), sys.setrecursionlimit(10000000)
+solve = functools.lru_cache(maxsize=10000000)(lambda i, j: 1e24 if j == 0 else (min(pow((values[-1] - values[i]),2),*tuple(pow(values[i] - values[x-1], 2) + solve(x, j-1) for x in range(i+1, n))) if i+1 != n else pow((values[-1] - values[i]),2)))
 print(solve(0, b))
