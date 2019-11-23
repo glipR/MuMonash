1c065b5a66e55fbcf729c4c7bb7cd2712390b4f6
glipR <jgoe0001@student.monash.edu>
2019-11-24 09:00:14 +1100

    2 Lines lets go.

---
+++ ICPCReg19/J/J.py
@@ -0,0 +1,2 @@
+import functools
+print(functools.reduce(lambda x, y: [x[0] + max(x[1] + 1, y[0] - y[1]), max(x[1] + 1, y[0] - y[1])], sorted([tuple(map(int, input().split())) for x in range(int(input()))], key=lambda x: (x[0] - x[1]) * 1e10 + x[0] + x[1]), [0, 0])[0])
