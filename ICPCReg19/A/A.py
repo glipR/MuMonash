59ceab5fce6c168ba779f56d840cf8d929a2830c
glipR <jgoe0001@student.monash.edu>
2019-11-24 17:25:07 +1100

    Fix A.

--- ICPCReg19/A/A.py
+++ ICPCReg19/A/A.py
@@ -1,3 +1,3 @@
 from functools import *
 n,(d, f),(e, g),v=int(input()),map(int,input().split()),map(int,input().split()),lru_cache()(lambda n:reduce(lambda a,z:(a[0]+z//2,a[1]+z%2),(int(input()) for _ in range(n)),[0,0]))
-print("Yes" if(max(d-v(n)[0],0)+max(e-v(n)[0],0)<=v(n)[1] and 2*v(n)[0]+v(n)[1]<=f+g) else "No")
+print("Yes" if(v(n)[0]<=min(f, g) and max(d-v(n)[0],0)+max(e-v(n)[0],0)<=v(n)[1] and 2*v(n)[0]+v(n)[1]<=f+g) else "No")
