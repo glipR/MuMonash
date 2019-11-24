399e17a97c8c00085d1d89889f0ccc155084224f
glipR <jgoe0001@student.monash.edu>
2019-11-24 17:21:34 +1100

    Fix J ordering problem (Still 1 line!!!).

--- ICPCReg19/J/J.py
+++ ICPCReg19/J/J.py
@@ -1,2 +1,2 @@
 import functools
-print(' '.join(map(str, functools.reduce(lambda x,y:[x[0]+max(x[1]+1,y[0]-y[1]),max(x[1]+1,y[0]-y[1]),x[2]+[max(x[1]+1,y[0]-y[1])]],sorted([tuple(map(int,input().split())) for x in range(int(input()))],key=lambda x:(x[0]-x[1])*1e10+x[0]+x[1]),[0,0,[]])[2])))
+print(' '.join(map(lambda x: str(x[1]),sorted(functools.reduce(lambda x,y:[x[0]+max(x[1]+1,y[1]-y[2]),max(x[1]+1,y[1]-y[2]),x[2]+[[y[0], max(x[1]+1,y[1]-y[2])]]],sorted([tuple((x, *map(int,input().split()))) for x in range(int(input()))],key=lambda x:(x[1]-x[2])*1e10+x[1]+x[2]),[0,0,[]])[2],key=lambda x:x[0]))))
