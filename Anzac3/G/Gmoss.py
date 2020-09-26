e9c84b76031f6251463e0385631672033e298366
leaen <mossbanay@gmail.com>
2020-09-26 12:41:30 +1000

    f nzpc 1st sub

--- Anzac3/G/Gmoss.py
+++ Anzac3/G/Gmoss.py
@@ -1,46 +1,59 @@
-a = [1 if x=='B' else -1 for x in input()]
+DEBUG = True
+DEBUG = False
+
+a = []
+for c in input():
+    if c == 'B':
+        a.append(1)
+    elif c == 'R':
+        a.append(-1)
+#a = [1 if x=='B' else -1 for x in input()]
 b = [x * -1 for x in a]
+
+if DEBUG:
+    print(f'a: {a}')
+    print(f'b: {b}')
 
 def solve(arr):
     best = -100
     best_range = None
-    cur = -100
-
+    cur = 0
     left = 0
-    right = 0
 
-    for i, x in enumerate(arr):
-        cur += x
-
-        if cur > 0:
-            right += 1
+    for right, x in enumerate(arr):
+        if cur >= 0:
+            cur += x
         else:
-            left = i
-            right = i
+            left = right
             cur = x
 
         if cur > best:
             best = cur
             best_range = (left, right)
 
-        #print(f'\ni: {i}')
-        #print(f'x: {x}')
-        #print(f'cur: {cur}')
-        #print(f'best: {best}')
-        #print(f'best_range: {best_range}')
+        if DEBUG:
+            print(f'\ni: {i}')
+            print(f'x: {x}')
+            print(f'cur: {cur}')
+            print(f'range: {(left, right)}')
+            print(f'best: {best}')
+            print(f'best_range: {best_range}')
 
     return (best, best_range)
 
 a1, ar1 = solve(a)
 b1, br1 = solve(b)
 
-#print(f'a1: {a1}')
-#print(f'ar1: {ar1}')
-#print(f'b1: {b1}')
-#print(f'br1: {br1}')
+if DEBUG:
+    print(f'a1: {a1}')
+    print(f'ar1: {ar1}')
+    print(f'b1: {b1}')
+    print(f'br1: {br1}')
 
 if a1 == b1:
-    if ar1[0] < br1[0]:
+    if ar1[0] == br1[0]:
+        assert False
+    elif ar1[0] < br1[0]:
         l, r = ar1
         l += 1
         r += 1
@@ -50,14 +63,15 @@ if a1 == b1:
         l += 1
         r += 1
         print(l, r)
+elif a1 > b1:
+    l, r = ar1
+    l += 1
+    r += 1
+    print(l, r)
+elif b1 > a1:
+    l, r = br1
+    l += 1
+    r += 1
+    print(l, r)
 else:
-    if a1 > b1:
-        l, r = ar1
-        l += 1
-        r += 1
-        print(l, r)
-    else:
-        l, r = br1
-        l += 1
-        r += 1
-        print(l, r)
+    assert False
---
+++ NZPC20/F/F.py
@@ -0,0 +1,75 @@
+from collections import deque
+
+DEBUG = True
+
+SEEKING = 0
+IN_TAG = 1
+
+line = input().strip()
+while line != '#':
+    s = deque()
+    idx = 0
+
+    illegal = False
+
+    while idx < len(line):
+        if line[idx] != '<':
+            idx += 1
+            continue
+
+        # We are at the start of a tag, parse the tag name by taking whitespace until
+        # a space or \ or >
+
+        inside_tag = ""
+        idx += 1
+
+        while idx < len(line) and line[idx] != '>':
+            inside_tag += line[idx]
+            idx += 1
+
+        if idx == len(line):
+            # At end of line with no closing >
+            if DEBUG:
+                print('no closing >, parsed {inside_tag}')
+
+            illegal = True
+            break
+        else:
+            assert line[idx] == '>'
+            idx += 1
+
+        tag_name = inside_tag.split(' ')[0]
+        if tag_name.startswith('/'):
+            # closing tag
+            tag_name = tag_name[1:]
+            if len(s) == 0:
+                if DEBUG:
+                    print(f'closing tag {tag_name} with empty tag')
+                illegal = True
+                break
+
+            pairing_tag_name = s.pop()
+            if tag_name != pairing_tag_name:
+                if DEBUG:
+                    print(f'mismatch of tags, opened with {pairing_tag_name} closed with {tag_name}')
+                illegal = True
+                break
+        elif inside_tag.endswith('/'):
+            # self closing tag
+            pass
+
+        else:
+            # new tag
+            s.append(tag_name)
+
+    if len(s) != 0:
+        if DEBUG:
+            print(f'stack not empty: {str(s)}')
+        illegal = True
+
+    if illegal:
+        print('illegal')
+    else:
+        print('legal')
+
+    line = input().strip()
