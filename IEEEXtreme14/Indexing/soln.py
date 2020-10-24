775c8fcff25780c6daba31475fe286600e74dd86
moss <mossbanay@gmail.com>
2020-10-24 13:46:38 +1100

    use tail of elements

--- IEEEXtreme14/Indexing/soln.py
+++ IEEEXtreme14/Indexing/soln.py
@@ -1,13 +1,26 @@
 import xml.etree.ElementTree as ET
 from collections import Counter
 import sys
+from decimal import Decimal
 
 sys.setrecursionlimit(20000)
 
 n_words = 0
 
+def _assert(cond):
+    if cond:
+        pass
+    else:
+        import sys
+        sys.exit(1)
+
 def token_to_word(token):
     token = token.lower()
+    for c in ["<", ">", "/", "\\"]:
+        if c in token:
+            import sys
+            sys.exit(1)
+            
     for c in [",", ".", "?", "!"]:
         while c in token:
             token = token.replace(c, "")
@@ -25,28 +38,32 @@ index_terms = input().strip().split(';')
 raw_xml_doc = ""
 line = input()
 while line is not None:
-    raw_xml_doc += line.strip() + " "
+    raw_xml_doc += " " + line.replace("\n", " ") + " "
     
     try:
         line = input()
     except EOFError:
         break
 
-tree = ET.fromstring(f'<root>{raw_xml_doc}</root>')
+tree = ET.fromstring(f'<root> {raw_xml_doc} </root>')
 
 def count_element(element, weight=0):
     global n_words
     
     if element.tag == "title":
+        _assert(weight == 0)
         weight = 5
     elif element.tag == "abstract":
+        _assert(weight == 0)
         weight = 3
     elif element.tag == "body":
+        _assert(weight == 0)
         weight = 1
     
     words = []
-    if element.text is not None:
-        words = line_to_words(element.text)
+    raw_text = (element.text or "") + (element.tail or "")
+    if raw_text != "":
+        words = line_to_words(raw_text)
         if weight > 0:
             n_words += len(words)
     c = Counter(words*weight)
@@ -62,14 +79,19 @@ all_words = count_element(tree)
 
 # print(f'n_words={n_words}')
 
+# print(index_terms)
+
 densities = [(-all_words[w], w) for w in index_terms if all_words[w]>0]
 n_printed = 0
 last_count = None
 
+# print(densities)
+# print(all_words)
+
 for count, w in sorted(densities):
     count = -count
     if last_count is None or last_count == count or n_printed < 3:
-        d = 100 * (count / n_words)
+        d = str(Decimal(100) * Decimal(count) / Decimal(n_words))
         print(f"{w}: {d}")
         last_count = count
         n_printed += 1
